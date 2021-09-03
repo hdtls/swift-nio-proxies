481460cc00e9540b956e5ed859bd74639d8d6a15
Junfeng Zhang <jeffzx15o@gmail.com>
2021-09-03 17:56:33 +0800

    Fix bug that peer do not closed when error.
    Motivation:
    
    Peer should close when error occurred.
    
    Modifications:
    
    * MODIFIED HTTP1ServerCONNECTTunnelHandler.swift
    
    Result:
    
    Bug fixed.

--- Sources/HTTP/Channel Handlers/HTTP1ServerCONNECTTunnelHandler.swift
+++ Sources/HTTP/Channel Handlers/HTTP1ServerCONNECTTunnelHandler.swift
@@ -10,17 +10,17 @@ public final class HTTP1ServerCONNECTTunnelHandler: ChannelInboundHandler, Remov
     
     private var state: ServerStateMachine
     
-        /// The task uri for truly http request. this value is updated after  `head` part received.
+    /// The task uri for truly http request. this value is updated after  `head` part received.
     private var uri: String?
     
     /// When a proxy request is received, we will send a new request to the target server.
     /// During the request is established, we need to cache the proxy request data.
-    private var readBuffers = MarkedCircularBuffer<NIOAny>.init(initialCapacity: 2)
-            
+    private var readBuffers: CircularBuffer<NIOAny> = .init()
+    
     public let logger: Logger
-        
+    
     public let completion: (String) -> EventLoopFuture<Channel>
-        
+    
     public init(logger: Logger = .init(label: "com.netbot.http-tunnel-logging"), completion: @escaping (String) -> EventLoopFuture<Channel>) {
         self.logger = logger
         self.completion = completion
@@ -33,6 +33,7 @@ public final class HTTP1ServerCONNECTTunnelHandler: ChannelInboundHandler, Remov
     
     public func channelActive(context: ChannelHandlerContext) {
         beginHandshake(context: context)
+        context.fireChannelActive()
     }
     
     public func channelRead(context: ChannelHandlerContext, data: NIOAny) {
@@ -53,13 +54,21 @@ public final class HTTP1ServerCONNECTTunnelHandler: ChannelInboundHandler, Remov
         }
     }
     
-    public func handlerRemoved(context: ChannelHandlerContext) {
-            // We're being removed from the pipeline. If we have buffered events, deliver them.
+    public func removeHandler(context: ChannelHandlerContext, removalToken: ChannelHandlerContext.RemovalToken) {
+        defer {
+            context.leavePipeline(removalToken: removalToken)
+        }
+        
         guard state.proxyEstablished, !readBuffers.isEmpty else {
             return
         }
-        emptyInboundBuffer(context: context)
+        
+        // We're being removed from the pipeline. If we have buffered events, deliver them.
+        while !readBuffers.isEmpty {
+            context.fireChannelRead(readBuffers.removeFirst())
+        }
     }
+    
 }
 
 extension HTTP1ServerCONNECTTunnelHandler {
@@ -96,13 +105,20 @@ extension HTTP1ServerCONNECTTunnelHandler {
     }
     
     private func handleHTTPEndPartReceive(_ headers: HTTPHeaders?, context: ChannelHandlerContext) throws {
-            // New request is complete. We don't want any more data from now on.
+        // New request is complete. We don't want any more data from now on.
         context.pipeline.handler(type: ByteToMessageHandler<HTTPRequestDecoder>.self)
             .whenSuccess { httpDecoder in
                 context.pipeline.removeHandler(httpDecoder, promise: nil)
             }
         
-        let client = completion(uri!)
+        guard let uri = uri else {
+            // TODO: Invalid uri error handling
+            deliverOneError(HTTPProxyError.unexpectedRead, context: context)
+            return
+        }
+        
+        let client = completion(uri)
+        logger.info("connecting to proxy server...")
         
         client.whenSuccess { channel in
             self.handleRemoteConnect(peerChannel: channel, context: context)
@@ -114,53 +130,42 @@ extension HTTP1ServerCONNECTTunnelHandler {
     }
     
     private func handleRemoteConnect(peerChannel: Channel, context: ChannelHandlerContext) {
+        logger.info("proxy server connected \(peerChannel.remoteAddress?.description ?? "")")
+        
         do {
             try state.sendServerGreeting()
         } catch {
             deliverOneError(error, context: context)
         }
         
-            // Ok, upgrade has completed! We now need to begin the upgrade process.
-            // First, send the 200 message.
-            // This content-length header is MUST NOT, but we need to workaround NIO's insistence that we set one.
+        logger.info("sending establish message to \(String(describing: context.channel.localAddress))...")
+        // Ok, upgrade has completed! We now need to begin the upgrade process.
+        // First, send the 200 message.
+        // This content-length header is MUST NOT, but we need to workaround NIO's insistence that we set one.
         let headers = HTTPHeaders([("Content-Length", "0")])
         let head = HTTPResponseHead(version: .http1_1, status: .ok, headers: headers)
-        logger.info("sending establish message to: \(peerChannel.remoteAddress!)...")
-        
         context.write(wrapOutboundOut(.head(head)), promise: nil)
         context.writeAndFlush(wrapOutboundOut(.end(nil)), promise: nil)
         
-        context.pipeline.handler(type: HTTPResponseEncoder.self)
-            .whenSuccess { httpEncoder in
-                context.pipeline.removeHandler(httpEncoder, promise: nil)
-            }
-        
-            // Now we need to glue our channel and the peer channel together.
         let (localGlue, peerGlue) = GlueHandler.matchedPair()
-        context.channel.pipeline.addHandler(localGlue)
-            .and(peerChannel.pipeline.addHandler(peerGlue))
-            .whenComplete { result in
-                switch result {
-                    case .success(_):
-                        context.pipeline.removeHandler(self, promise: nil)
-                        self.emptyInboundBuffer(context: context)
-                    case .failure(let error):
-                            // Close connected peer channel before closing our channel.
-                        self.deliverOneError(error, context: context)
-                }
-            }
-    }
-    
-    private func emptyInboundBuffer(context: ChannelHandlerContext) {
-        while readBuffers.hasMark {
-            let buffer = readBuffers.removeFirst()
-            context.write(buffer, promise: nil)
-        }
-        context.flush()
         
-        while !readBuffers.isEmpty {
-            context.write(readBuffers.removeFirst(), promise: nil)
-        }
+        context.pipeline.handler(type: HTTPResponseEncoder.self)
+            .flatMap {
+                context.pipeline.removeHandler($0)
+            }
+            .flatMap {
+                // Now we need to glue our channel and the peer channel together.
+                context.channel.pipeline.addHandler(localGlue)
+                    .and(peerChannel.pipeline.addHandler(peerGlue))
+            }
+            .flatMap { _ in
+                context.pipeline.removeHandler(self)
+            }
+            .whenFailure { error in
+                // Close connected peer channel before closing our channel.
+                peerChannel.close(mode: .all, promise: nil)
+                self.deliverOneError(error, context: context)
+            }
     }
     
     private func deliverOneError(_ error: Error, context: ChannelHandlerContext) {
@@ -169,4 +174,8 @@ extension HTTP1ServerCONNECTTunnelHandler {
         context.fireErrorCaught(error)
     }
     
+    public func errorCaught(context: ChannelHandlerContext, error: Error) {
+        logger.error("\(error)")
+        context.fireErrorCaught(error)
+    }
 }
