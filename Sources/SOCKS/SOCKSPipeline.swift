6384c8f8ae5623fc25fb5fbf6f9ff1e2e67057c5
Junfeng Zhang <jeffzx15o@gmail.com>
2021-09-06 14:30:02 +0800

    Add SOCKS pipeline setup.
    Motivation:
    
    Make SOCKS pipeline setup simpler.
    
    Modifications:
    
    * ADD SOCKSPipeline file.
    
    Result:
    
    Use addSOCKSClientHandlers(logger:taskAddress:credential:position:) to setup pipeline.

---
+++ Sources/SOCKS/SOCKSPipeline.swift
@@ -0,0 +1,57 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Netbot open source project
+//
+// Copyright (c) 2021 Junfeng Zhang. and the Netbot project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE for license information
+// See CONTRIBUTORS.txt for the list of Netbot project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import NIOCore
+import Logging
+import Helpers
+
+extension ChannelPipeline {
+    
+    public func addSOCKSClientHandlers(logger: Logger = .init(label: "com.netbot.socks"),
+                                    taskAddress: NetAddress,
+                                    credential: Credential?,
+                                    position: Position = .last) -> EventLoopFuture<Void> {
+        let eventLoopFuture: EventLoopFuture<Void>
+        
+        if eventLoop.inEventLoop {
+            let result = Result<Void, Error> {
+                try syncOperations.addSOCKSClientHandlers(logger: logger,
+                                                       taskAddress: taskAddress,
+                                                          credential: credential,
+                                                       position: position)
+            }
+            eventLoopFuture = eventLoop.makeCompletedFuture(result)
+        } else {
+            eventLoopFuture = eventLoop.submit({
+                try self.syncOperations.addSOCKSClientHandlers(logger: logger,
+                                                            taskAddress: taskAddress,
+                                                               credential: credential,
+                                                            position: position)
+            })
+        }
+        
+        return eventLoopFuture
+    }
+}
+
+extension ChannelPipeline.SynchronousOperations {
+    
+    public func addSOCKSClientHandlers(logger: Logger = .init(label: "com.netbot.socks"),
+                                    taskAddress: NetAddress,
+                                    credential: Credential?,
+                                    position: ChannelPipeline.Position = .last) throws {
+        eventLoop.assertInEventLoop()
+        try addHandler(SOCKS5ClientHandler(credential: credential, targetAddress: taskAddress))
+    }
+}
