cdb1c4e39b4c7fa6e4d108c578a8b9027257be9b
Junfeng Zhang <jeffzx15o@gmail.com>
2021-09-23 19:12:19 +0800

    Add IO transformer for MitM pipe.
    Motivation:
    
    MitM decode request to HTTPart<HeadT, ByteBuffer> but http client need HTTPPart<HeadT, IOData>
    so we add this handler to transfer inbound.
    
    Modifications:
    
    * Add HTTPIOTransformer
    
    Result:
    
    MitM server can use HTTPIOTransformer to transfer inbound.

---
+++ Sources/HTTP/HTTPIOTransformer.swift
@@ -0,0 +1,34 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Netbot open source project
+//
+// Copyright (c) 2021 Junfeng Zhang. and the Netbot project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE for license information
+// See CONTRIBUTORS.txt for the list of Netbot project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import NIOCore
+import NIOHTTP1
+
+/// Transfer inbound from `HTTPPart<HeadT, ByteBuffer>` to `HTTPPart<HeadT, IOData>`.
+public final class HTTPIOTransformer<HeadT: Equatable>: ChannelInboundHandler {
+    
+    public typealias InboundIn = HTTPPart<HeadT, ByteBuffer>
+    public typealias InboundOut = HTTPPart<HeadT, IOData>
+        
+    public func channelRead(context: ChannelHandlerContext, data: NIOAny) {
+        switch unwrapInboundIn(data) {
+            case .head(let head):
+                context.fireChannelRead(wrapInboundOut(.head(head)))
+            case .body(let byteBuffer):
+                context.fireChannelRead(wrapInboundOut(.body(.byteBuffer(byteBuffer))))
+            case .end(let trailers):
+                context.fireChannelRead(wrapInboundOut(.end(trailers)))
+        }
+    }
+}
