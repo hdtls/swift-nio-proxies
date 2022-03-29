ad90f74b6fd045099af216b78d0e10a4743beb66
Junfeng Zhang <jeffzx15o@gmail.com>
2022-03-29 18:58:33 +0800

    Fix SS crash when increment nonce.
    Motivation
    Crash fix.
    
    Modification
    - Update SS nonce.
    
    Result
    Use truncatingIfNeeded to fix bits error.

--- Sources/NetbotSS/Nonce.swift
+++ Sources/NetbotSS/Nonce.swift
@@ -19,11 +19,12 @@ extension Array where Element == UInt8 {
     /// Increment array like `sodium_increment(_:)`
     /// - Returns: result value
     mutating func increment(_ length: Int) {
-        var c: UInt8 = 1
-        self = map { e in
-            c += e
+        var c: UInt16 = 1
+        
+        self = self.map { e in
+            c += UInt16(e)
             defer { c >>= 8 }
-            return c & 0xFF
+            return UInt8(truncatingIfNeeded: c)
         }
     }
 }
