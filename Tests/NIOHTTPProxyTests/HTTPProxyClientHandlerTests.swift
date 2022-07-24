12cb5413931be36ee91c2b9fd987cb7c00d4984a
Junfeng Zhang <jeffzx15o@gmail.com>
2022-07-24 22:45:15 +0800

    Test http proxy client handler.
    Motivation
    Unit tests.
    
    Modification
    - Update HTTPProxyClientHandler.
    
    Result
    Strongify programm.

--- Tests/NIOHTTPProxyTests/HTTPProxyClientHandlerTests.swift
+++ Tests/NIOHTTPProxyTests/HTTPProxyClientHandlerTests.swift
@@ -13,18 +13,13 @@
 //===----------------------------------------------------------------------===//
 
 import NIO
+import NIOHTTPProxy
 import XCTest
-
-@testable import NIOHTTPProxy
 
 class HTTPProxyClientHandlerTests: XCTestCase {
 
     var channel: EmbeddedChannel!
     var handler: HTTP1ClientCONNECTTunnelHandler!
-
-    func connect() {
-        try! self.channel.connect(to: .init(ipAddress: "127.0.0.1", port: 80)).wait()
-    }
 
     override func setUpWithError() throws {
         XCTAssertNil(self.channel)
@@ -35,46 +30,234 @@ class HTTPProxyClientHandlerTests: XCTestCase {
             passwordReference: "passwordReference",
             authenticationRequired: false,
             preferHTTPTunneling: true,
-            destinationAddress: .socketAddress(try .init(ipAddress: "127.0.0.1", port: 8080))
+            destinationAddress: .domainPort("swift.org", 443)
         )
-        self.channel = EmbeddedChannel(handler: self.handler)
+
+        self.channel = EmbeddedChannel()
+        try self.channel.pipeline.syncOperations.addHTTPClientHandlers()
+        try self.channel.pipeline.syncOperations.addHandler(handler)
     }
 
-    override func tearDownWithError() throws {
+    override func tearDown() {
         XCTAssertNotNil(self.channel)
         self.channel = nil
     }
 
-    func assertHTTPPart(_ httpPart: HTTPClientRequestPart?, line: UInt = #line) throws {
-        let part = try self.channel.readOutbound(as: HTTPClientRequestPart.self)
-        XCTAssertEqual(part, httpPart, line: line)
+    func waitUtilConnected() throws {
+        try self.channel.connect(to: .init(ipAddress: "127.0.0.1", port: 80)).wait()
     }
 
-    func testDelayedConnection() throws {
-        XCTAssertNil(try self.channel.readOutbound())
-
-        self.connect()
-
-        try assertHTTPPart(.head(.init(version: .http1_1, method: .CONNECT, uri: "127.0.0.1:8080")))
-
-        try assertHTTPPart(.end(nil))
-    }
-
-    func testDelayedHandlerAdded() throws {
-        XCTAssertNoThrow(try self.channel.close().wait())
-
-        self.channel = EmbeddedChannel()
-        XCTAssertNoThrow(
-            try self.channel.connect(to: .init(ipAddress: "127.0.0.1", port: 80)).wait()
+    func testHandshakingShouldBeginAfterChannelActive() throws {
+        XCTAssertFalse(channel.isActive)
+        XCTAssertNil(try channel.readOutbound())
+        try waitUtilConnected()
+        XCTAssertTrue(channel.isActive)
+        XCTAssertEqual(
+            try channel.readOutbound(),
+            ByteBuffer(string: "CONNECT swift.org:443 HTTP/1.1\r\n\r\n")
         )
+    }
+
+    func testAddHandlerAfterChannelActive() throws {
+        XCTAssertNoThrow(try self.channel.close().wait())
+        self.channel = EmbeddedChannel()
+        XCTAssertNoThrow(try waitUtilConnected())
         XCTAssertTrue(self.channel.isActive)
-
         XCTAssertNil(try self.channel.readOutbound())
-
+        XCTAssertNoThrow(try channel.pipeline.syncOperations.addHTTPClientHandlers())
         XCTAssertNoThrow(self.channel.pipeline.addHandler(self.handler))
+        XCTAssertEqual(
+            try channel.readOutbound(),
+            ByteBuffer(string: "CONNECT swift.org:443 HTTP/1.1\r\n\r\n")
+        )
+        XCTAssertNoThrow(try channel.finish())
+    }
 
-        try assertHTTPPart(.head(.init(version: .http1_1, method: .CONNECT, uri: "127.0.0.1:8080")))
+    func testBuffering() throws {
+        try waitUtilConnected()
 
-        try assertHTTPPart(.end(nil))
+        let writePromise = self.channel.eventLoop.makePromise(of: Void.self)
+        channel.writeAndFlush(ByteBuffer(bytes: [1, 2, 3]), promise: writePromise)
+        XCTAssertEqual(
+            try channel.readOutbound(),
+            ByteBuffer(string: "CONNECT swift.org:443 HTTP/1.1\r\n\r\n")
+        )
+        try channel.writeInbound(ByteBuffer(string: "HTTP/1.1 200 OK\r\n\r\n"))
+        channel.embeddedEventLoop.advanceTime(to: .now())
+
+        XCTAssertNoThrow(try writePromise.futureResult.wait())
+        XCTAssertEqual(try channel.readOutbound(), ByteBuffer(bytes: [1, 2, 3]))
+        XCTAssertNoThrow(try channel.finish())
+    }
+
+    func testBufferingWithMark() throws {
+        try waitUtilConnected()
+        let writePromise1 = self.channel.eventLoop.makePromise(of: Void.self)
+        let writePromise2 = self.channel.eventLoop.makePromise(of: Void.self)
+        channel.write(ByteBuffer(bytes: [1, 2, 3]), promise: writePromise1)
+        channel.flush()
+        channel.write(ByteBuffer(bytes: [4, 5, 6]), promise: writePromise2)
+
+        XCTAssertEqual(
+            try channel.readOutbound(),
+            ByteBuffer(string: "CONNECT swift.org:443 HTTP/1.1\r\n\r\n")
+        )
+        try channel.writeInbound(ByteBuffer(string: "HTTP/1.1 200 OK\r\n\r\n"))
+        channel.embeddedEventLoop.advanceTime(to: .now())
+
+        XCTAssertNoThrow(try writePromise1.futureResult.wait())
+        XCTAssertEqual(try channel.readOutbound(), ByteBuffer(bytes: [1, 2, 3]))
+
+        XCTAssertNotNil(try channel.writeAndFlush(ByteBuffer(bytes: [7, 8, 9])).wait())
+        XCTAssertEqual(try channel.readOutbound(), ByteBuffer(bytes: [4, 5, 6]))
+        XCTAssertEqual(try channel.readOutbound(), ByteBuffer(bytes: [7, 8, 9]))
+        XCTAssertNoThrow(try channel.finish())
+    }
+
+    func testBasicAuthenticationSuccess() throws {
+        try channel.close().wait()
+
+        handler = .init(
+            logger: .init(label: ""),
+            username: "username",
+            passwordReference: "passwordReference",
+            authenticationRequired: true,
+            preferHTTPTunneling: true,
+            destinationAddress: .domainPort("swift.org", 443)
+        )
+
+        channel = EmbeddedChannel()
+        try channel.pipeline.syncOperations.addHTTPClientHandlers()
+        try channel.pipeline.syncOperations.addHandler(handler)
+
+        try waitUtilConnected()
+
+        let writePromise = self.channel.eventLoop.makePromise(of: Void.self)
+        channel.writeAndFlush(ByteBuffer(bytes: [1, 2, 3]), promise: writePromise)
+
+        XCTAssertEqual(
+            try channel.readOutbound(),
+            ByteBuffer(
+                string:
+                    "CONNECT swift.org:443 HTTP/1.1\r\nproxy-authorization: Basic dXNlcm5hbWU6cGFzc3dvcmRSZWZlcmVuY2U=\r\n\r\n"
+            )
+        )
+        try channel.writeInbound(ByteBuffer(string: "HTTP/1.1 200 OK\r\n\r\n"))
+        channel.embeddedEventLoop.advanceTime(to: .now())
+
+        XCTAssertNoThrow(try writePromise.futureResult.wait())
+        XCTAssertEqual(try channel.readOutbound(), ByteBuffer(bytes: [1, 2, 3]))
+        XCTAssertNoThrow(try channel.finish())
+    }
+
+    func testBasicAuthenticationWithIncorrectUsernameOrPassword() throws {
+        try channel.close().wait()
+
+        handler = .init(
+            logger: .init(label: ""),
+            username: "username",
+            passwordReference: "passwordReference",
+            authenticationRequired: true,
+            preferHTTPTunneling: true,
+            destinationAddress: .domainPort("swift.org", 443)
+        )
+
+        channel = EmbeddedChannel()
+        try channel.pipeline.syncOperations.addHTTPClientHandlers()
+        try channel.pipeline.syncOperations.addHandler(handler)
+
+        try waitUtilConnected()
+
+        XCTAssertEqual(
+            try channel.readOutbound(),
+            ByteBuffer(
+                string:
+                    "CONNECT swift.org:443 HTTP/1.1\r\nproxy-authorization: Basic dXNlcm5hbWU6cGFzc3dvcmRSZWZlcmVuY2U=\r\n\r\n"
+            )
+        )
+        try channel.writeInbound(
+            ByteBuffer(string: "HTTP/1.1 407 Proxy Authentication Required\r\n\r\n")
+        )
+        channel.embeddedEventLoop.advanceTime(to: .now())
+
+        // TODO: Error handling
+    }
+
+    func testBasicAuthenticationRequired() throws {
+        try waitUtilConnected()
+
+        XCTAssertEqual(
+            try channel.readOutbound(),
+            ByteBuffer(string: "CONNECT swift.org:443 HTTP/1.1\r\n\r\n")
+        )
+        try channel.writeInbound(
+            ByteBuffer(string: "HTTP/1.1 407 Proxy Authentication Required\r\n\r\n")
+        )
+        channel.embeddedEventLoop.advanceTime(to: .now())
+
+        // TODO: Error handling
+    }
+
+    func testBasicAuthenticationRequired0() throws {
+        try waitUtilConnected()
+
+        XCTAssertEqual(
+            try channel.readOutbound(),
+            ByteBuffer(string: "CONNECT swift.org:443 HTTP/1.1\r\n\r\n")
+        )
+        try channel.writeInbound(ByteBuffer(string: "\r\n"))
+        try channel.writeInbound(
+            ByteBuffer(string: "HTTP/1.1 407 Proxy Authentication Required\r\n")
+        )
+        try channel.writeInbound(ByteBuffer(string: "\r\n"))
+        channel.embeddedEventLoop.advanceTime(to: .now())
+
+        // TODO: Error handling
+    }
+
+    func testHTTPEncoderAndDecoderShouldBeenRemovedAfterEstablishedEventTriggered() throws {
+        final class EventHandler: ChannelInboundHandler {
+            typealias InboundIn = NIOAny
+
+            let promise: EventLoopPromise<Void>
+
+            init(promise: EventLoopPromise<Void>) {
+                self.promise = promise
+            }
+
+            func userInboundEventTriggered(context: ChannelHandlerContext, event: Any) {
+                guard let e = event as? UserEvent else {
+                    context.fireUserInboundEventTriggered(event)
+                    return
+                }
+
+                switch e {
+                    case .established:
+                        promise.succeed(())
+                }
+            }
+        }
+
+        let promise = channel.eventLoop.makePromise(of: Void.self)
+        XCTAssertNoThrow(try channel.pipeline.addHandler(EventHandler(promise: promise)).wait())
+        try waitUtilConnected()
+
+        try channel.writeInbound(ByteBuffer(string: "HTTP/1.1 200 OK\r\n\r\n"))
+        channel.embeddedEventLoop.advanceTime(to: .now())
+
+        XCTAssertNoThrow(try promise.futureResult.wait())
+
+        XCTAssertThrowsError(
+            try self.channel.pipeline.handler(type: HTTPRequestEncoder.self).wait()
+        ) { error in
+            XCTAssertEqual(error as? ChannelPipelineError, ChannelPipelineError.notFound)
+        }
+
+        XCTAssertThrowsError(
+            try self.channel.pipeline.handler(type: ByteToMessageHandler<HTTPResponseDecoder>.self)
+                .wait()
+        ) { error in
+            XCTAssertEqual(error as? ChannelPipelineError, ChannelPipelineError.notFound)
+        }
     }
 }
